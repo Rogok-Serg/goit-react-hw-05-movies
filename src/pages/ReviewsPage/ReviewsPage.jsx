2ddc156996e362aeb8d0dd07d286f2c0f570375b
Rogok-Serg <rogoksv@gmail.com>
2023-07-18 21:21:14 +0300

    ++

--- src/pages/ReviewsPage/ReviewsPage.jsx
+++ src/pages/ReviewsPage/ReviewsPage.jsx
@@ -7,18 +7,14 @@ const Reviews = () => {
   const { movieId } = useParams();
   const apiReviews = `/movie/${movieId}/reviews`;
 
-  // const apiDataMovie = `/movie/${movieId}`;
   const [movieReviews, setMovieReviews] = useState([]);
-
   const [errorMes, setError] = useState(null);
-  // const [page, setPage] = 1;
 
   useEffect(() => {
     if (!movieId) return;
     const dataMovies = async () => {
       try {
         const data = await onFetchMoviesDetalis(apiReviews);
-        // const { reviews } = data;
         setMovieReviews(data.results);
       } catch (error) {
         setError(error.message);
@@ -29,12 +25,12 @@ const Reviews = () => {
   }, [apiReviews, errorMes, movieId]);
   return (
     <div>
-      {movieReviews.langth > 0 ? (
-        <div>
-          <p>Not Movies</p>
-        </div>
-      ) : (
+      {movieReviews.length > 0 ? (
         <ReviewsList reviews={movieReviews} />
+      ) : (
+        <div>
+          <p>We don`t have any reviews for this movie.</p>
+        </div>
       )}
     </div>
   );
