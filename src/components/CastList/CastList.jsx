27d07dea1b86807695dfb7950cf8fcf363cfc92f
Rogok-Serg <rogoksv@gmail.com>
2023-07-18 21:24:19 +0300

    fix cast

--- src/components/CastList/CastList.jsx
+++ src/components/CastList/CastList.jsx
@@ -4,14 +4,14 @@ const CastList = ({ cast }) => {
     <ul>
       {cast.map(el => {
         const { id, name, profile_path } = el;
+        const poster = profile_path
+          ? `https://image.tmdb.org/t/p/w400${profile_path}`
+          : 'https://dummyimage.com/200x300/99cccc.gifffffff&text=No%20poster!';
+
         return (
           <li key={id}>
             {' '}
-            <img
-              src={`https://image.tmdb.org/t/p/w300${profile_path}`}
-              alt={name}
-              width={200}
-            />
+            <img src={poster} alt={name} width={200} />
             <p>{name}</p>
           </li>
         );
